1413d4f5724a0a7ff5ddfd65cc33ff7e5f8d4d3e
JoacimBlitzner <blitzner.joacim@gmail.com>
2023-03-22 10:39:36 +0100

    första commit

---
+++ Workschedule/src/main/java/com/patrik/joacim/Workschedule/WorkscheduleApplication.java
@@ -0,0 +1,13 @@
+package com.patrik.joacim.Workschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkscheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkscheduleApplication.class, args);
+	}
+
+}
